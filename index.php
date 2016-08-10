a035c7ef6cdbbd2a3c9253b864498b4347a2017c
JahsonK <kimulukyalo@gmail.com>
2016-08-10 11:14:05 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Simple PHP Web Crawler</title>
+<link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+<body>
+<form method="post" action="crawler.php">
+<p style="text-align:center;"><input type="text" name="target" class="input" placeholder="Type site url here" /></p>
+<p style="text-align:center;"><input type="submit" name="Crawl Site" class="button" value="Crawl !" /></p>
+</form>
+</body>
+</html>
